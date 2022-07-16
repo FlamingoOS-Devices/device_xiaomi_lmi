c4f3d29eb3d1cfb4ef63d1b2ca8d04687aa175d9
ZenkaBestia <z.mail.6600@gmail.com>
2022-07-16 07:25:54 +0000

    Revert "lmi: parts: Initial popup camera impl"
    This reverts commit 5fb9f3db92c307a2310b661e08fb1d71570b83c2.

--- parts/src/org/lineageos/devicesettings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/devicesettings/BootCompletedReceiver.java
@@ -22,8 +22,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import org.lineageos.devicesettings.popupcamera.PopupCameraUtils;
-
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
@@ -32,6 +30,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        PopupCameraUtils.startService(context);
     }
 }
